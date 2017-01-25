a46cecb8d2ab182c4eec66e3076a4d832152a9fb
Laure <laure.milian@gmail.com>
2017-01-25 16:56:22 +0100

    introduction function backword, test check

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -29,10 +29,38 @@ test('The rover moves forward two times', function() {
 	expect(instance.direction).toBe('E');
 })
 
-test('The rover turn right one time', function() {
+test('The rover turns right one time', function() {
 	const instance = app();
 	instance.right();
 	expect(instance.x).toBe(0);
 	expect(instance.y).toBe(0);
 	expect(instance.direction).toBe('S');
+});
+
+test('The rover turns right two times', function() {
+	const instance = app();
+	instance.right();
+	instance.right();
+	expect(instance.x).toBe(0);
+	expect(instance.y).toBe(0);
+	expect(instance.direction).toBe('W');
+});
+
+test('The rover moves forward one time, turns right and moves forward two times', function() {
+	const instance = app();
+	instance.forward();
+	instance.right();
+	instance.forward();
+	instance.forward();
+	expect(instance.x).toBe(1);
+	expect(instance.y).toBe(-2);
+	expect(instance.direction).toBe('S');
+});
+
+test('The rover moves backward one time', function() {
+	const instance = app();
+	instance.backward();
+	expect(instance.x).toBe(-1);
+	expect(instance.y).toBe(0);
+	expect(instance.direction).toBe('E');
 });
--- index.js
+++ index.js
@@ -24,8 +24,38 @@ module.exports = function() {
 			}
 		},
 
+		backward: function() {
+			if (this.direction === 'E') {
+				this.x = this.x -1;
+				this.y = this.y;
+			}
+			else if (this.direction === 'S') {
+				this.x = this.x;
+				this.y = this.y +1;
+			}
+			else if (this.direction === 'W') {
+				this.x = this.x +1;
+				this.y = this.y;
+			}
+			else if (this.direction === 'N') {
+				this.x = this.x;
+				this.y = this.y -1;
+			}
+		},
+
 		right: function() {
-			this.direction = 'S';
+			if (this.direction === 'E') {
+				this.direction = 'S';
+			}
+			else if (this.direction === 'S') {
+				this.direction = 'W';
+			}
+			else if (this.direction === 'W') {
+				this.direction = 'N';
+			}
+			else if (this.direction === 'N') {
+				this.direction = 'E';
+			}
 		}
 
 	}
